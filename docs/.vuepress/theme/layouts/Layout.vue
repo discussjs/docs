c6d5f098d24da0867a4d03a9e969f7f46152344e
Lete114 <me@imlete.cn>
2022-07-07 11:36:32 +0800

    chore: 更新 discuss 版本为 v1.0.3

--- docs/.vuepress/theme/layouts/Layout.vue
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -3,7 +3,7 @@
     <template #page-bottom>
       <div class="page-edit">
         <div id="Discuss-wrap"></div>
-        <script src="https://npm.elemecdn.com/discuss@1.0.2/dist/Discuss.js" ref="DiscussJS"></script>
+        <script src="https://npm.elemecdn.com/discuss@1.0.3/dist/Discuss.js" ref="DiscussJS"></script>
       </div>
     </template>
   </ParentLayout>
