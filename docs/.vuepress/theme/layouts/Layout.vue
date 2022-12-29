096ea2ccebf5ed4ce4d6f6f865ce0260c89817d7
Lete114 <me@imlete.cn>
2022-12-29 19:03:34 +0800

    chore: 更新 discuss 前端版本 v1.2.1

--- docs/.vuepress/theme/layouts/Layout.vue
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -4,7 +4,7 @@
       <div class="page-edit">
         <div id="Discuss-wrap"></div>
         <script
-          src="https://lib.baomitu.com/discuss/1.2.0/dist/discuss.js"
+          src="https://lib.baomitu.com/discuss/1.2.1/dist/discuss.js"
           ref="DiscussJS"
         ></script>
       </div>
